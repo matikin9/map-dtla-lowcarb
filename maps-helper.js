2ca0042e84e797f90e215069527004c8858947c5
Nina Kin <matikin9@gmail.com>
2015-08-11 14:48:53 -0700

    Reads JSON input & displays markers.  Updated data.

--- maps-helper.js
+++ maps-helper.js
@@ -1,6 +1,8 @@
 /*
  * Markers: https://sites.google.com/site/gmapsdevelopment/
 */
+var map;
+
 function initialize() {
 	var mapCanvas = document.getElementById('map-canvas');
 	var mapOptions = {
@@ -9,49 +11,8 @@ function initialize() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	
-	var map = new google.maps.Map(mapCanvas, mapOptions);
+	map = new google.maps.Map(mapCanvas, mapOptions);
 	
-	var iconBase = 'http://labs.google.com/ridefinder/images/';
-	
-	restaurant_list.forEach(function(r) {
-		var myLatLng = new google.maps.LatLng(r.lat, r.lng);
-		
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			icon: iconBase + 'mm_20_red.png',
-			title: r.name
-		});
-		
-		var contentString = '<div id="content">'+
-			'<div id="siteNotice">'+
-			'</div>'+
-			'<h1>' + r.name + '</h1>'+
-			'<div id="bodyContent">'+
-			r.description + 
-			'</div>'+
-			'</div>';
-		  
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});
-		
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-	});
-	
-	
-	
-	
-	
-	
-	
-	 
-
 }
-
-
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- sheets-helper.js
+++ sheets-helper.js
@@ -8,17 +8,60 @@ function Restaurant(obj) {
 
 var restaurant_list = [];
 
-var obj = $.getJSON("data/low-carb-data.json", function(data) {
-    var items = [];
-    $.each(data, function(key, val) {
-        var item = "key: " + key + ", val: " + val;
-       items.push(item + "<br />"); 
+$.getJSON("https://raw.githubusercontent.com/matikin9/map-dtla-lowcarb/master/data/low-carb-data.json", function (data) {
+    var items = data.restaurants;
+    
+    $.each(data, function (key, val) {
+        if (key == "restaurants") {
+            restaurant_list = val;
+            /*
+            var restaurant_objects = val;
+
+            $.each(val, function (k, v) {
+                var item = "key: " + k + ", val: " + v;
+                items.push(item + "<br />");
+            });
+            */
+            
+            var iconBase = 'http://labs.google.com/ridefinder/images/';
+	
+        	restaurant_list.forEach(function(r) {
+        		var myLatLng = new google.maps.LatLng(r.coordinates.latitude, r.coordinates.longitude);
+        		
+        		var marker = new google.maps.Marker({
+        			position: myLatLng,
+        			map: map,
+        			icon: iconBase + 'mm_20_red.png',
+        			title: r.name
+        		});
+        		
+        		var contentString = '<div id="content">'+
+        			'<div id="siteNotice">'+
+        			'</div>'+
+        			'<h1>' + r.name + '</h1>'+
+        			'<div id="bodyContent">'+
+        			r.description + '<br /><br />' + r.address +
+        			'</div>'+
+        			'</div>';
+        		  
+        		var infowindow = new google.maps.InfoWindow({
+        			content: contentString
+        		});
+        		
+        		google.maps.event.addListener(marker, 'click', function() {
+        			infowindow.open(map,marker);
+        		});
+        	});
+        }
+
     });
 });
 
-restaurant_data.forEach(function(item) {
+/*
+restaurant_data.forEach(function (item) {
     if (item.coordinates[0] != null && item.coordinates[1] != null) {
         var entry = new Restaurant(item);
         restaurant_list.push(entry);
     }
 });
+*/
