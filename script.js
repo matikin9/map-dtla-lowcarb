6fe0bddd56d9c20fe6f36b1f1849fbe80d65cd4a
Nina Kin <nina@ninakin.com>
2016-12-22 21:44:09 +0000

    Removed extra files.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-/*global firebase */
-/*global $*/
-/*global L*/
-/*global mymap*/
+/*
+ * Initialize Firebase
+ */
+var config = {
+    apiKey: "AIzaSyC8Gpmbx5n6ql6Bk05anfYpl7S3HEdnGDw",
+    authDomain: "map-dtla-lowcarb.firebaseapp.com",
+    databaseURL: "https://map-dtla-lowcarb.firebaseio.com",
+    storageBucket: "map-dtla-lowcarb.appspot.com",
+    messagingSenderId: "423669000304"
+};
+firebase.initializeApp(config);
 
-// Retrieve data and add markers to map
+/*
+ * Initialize Map
+ */
+L.mapbox.accessToken = 'pk.eyJ1IjoibWF0aWtpbjkiLCJhIjoiYjMyMjBjZTE4NWUxMDkxOWZjZjFjZWEzZTcxNDUxOTkifQ._ldFl3e17jCs7aWm6zMZ3Q';
+var mymap = L.map('map-display').setView([34.0522, -118.2437], 13);
+L.mapbox.styleLayer('mapbox://styles/matikin9/cim5bt1q100iy9jkpl7ff9d1h').addTo(mymap);
+
+/*
+ * Retrieve data and add to map
+ */
 firebase.database().ref('restaurants').once('value').then(function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
         var key = childSnapshot.key;
