6ad1ebbf0abc297241aee76b9cdbf3382556a527
Nina Kin <nina@ninakin.com>
2016-12-29 19:33:16 +0000

    implements google maps geocoding api

---
+++ firebase-helper.js
@@ -0,0 +1,11 @@
+/*
+ * Initialize Firebase
+ */
+var config = {
+    apiKey: "AIzaSyC8Gpmbx5n6ql6Bk05anfYpl7S3HEdnGDw",
+    authDomain: "map-dtla-lowcarb.firebaseapp.com",
+    databaseURL: "https://map-dtla-lowcarb.firebaseio.com",
+    storageBucket: "map-dtla-lowcarb.appspot.com",
+    messagingSenderId: "423669000304"
+};
+firebase.initializeApp(config);
