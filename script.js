89de8f16035873182a94d7026a20e9028f69e719
Nina Kin <nina@ninakin.com>
2017-01-27 00:57:07 +0000

    added firebase write for geocoded coordinates

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+/*global $*/
+/*global L*/
+/*global firebase*/
 
 /*
  * Initialize Map
@@ -6,6 +8,7 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibWF0aWtpbjkiLCJhIjoiYjMyMjBjZTE4NWUxMDkxOWZjZjFjZWEzZTcxNDUxOTkifQ._ldFl3e17jCs7aWm6zMZ3Q';
 var mymap = L.map('map-display').setView([34.0522, -118.2437], 13);
 L.mapbox.styleLayer('mapbox://styles/matikin9/cim5bt1q100iy9jkpl7ff9d1h').addTo(mymap);
+var markers = [];
 
 /*
  * Retrieve data and add to map
@@ -28,7 +31,11 @@ firebase.database().ref('restaurants').once('value').then(function(snapshot) {
         // Create marker and add to map.
         L.marker([childData.lat, childData.lng])
         	.bindPopup($restaurantInfo[0])
-        	.addTo(mymap);
+        	.addTo(mymap)
+        	.push(markers);
+        
+        $('div#data').append($restaurantInfo);
+        $('div#data').append('<a href="#" script="onclick(L.marker.openPopup">view</a>');
     });
 });
 
